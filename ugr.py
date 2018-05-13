87f2be5313bd40ef71d6e9d155d8565024380da2
Fernando Fernandez Galindo <ffernandezgalindo@triumf.ca>
2018-05-12 18:30:41 -0700

    Added more info to v update

--- ugr.py
+++ ugr.py
@@ -5,6 +5,7 @@ storage status information from various types of endpoints.
 
 v0.0.1 Works with cephS3 AdminAPI.
 v0.0.2 Added AWS list-type2 API to list all objects in bucket and add object size.
+       Added memcached format and upload each endpoint obtained.
 """
 from __future__ import print_function
 
