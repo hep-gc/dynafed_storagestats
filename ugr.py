db09be6e1975e1fe23cc46ea9593cdd55120d2da
Fernando Fernandez Galindo <ffernandezgalindo@triumf.ca>
2018-05-14 15:24:09 -0700

    v0.0.3 Added support for python3

--- ugr.py
+++ ugr.py
@@ -6,10 +6,11 @@ storage status information from various types of endpoints.
 v0.0.1 Works with cephS3 AdminAPI.
 v0.0.2 Added AWS list-type2 API to list all objects in bucket and add object size.
        Added memcached format and upload each endpoint obtained.
+v0.0.3 Added support for python3
 """
 from __future__ import print_function
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 __author__ = "Fernando Fernandez Galindo"
 
 import sys
