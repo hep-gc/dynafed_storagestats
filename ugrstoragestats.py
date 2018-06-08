f57f45fe7573429dcfc72a924fb79603ceb1dcc3
Fernando Fernandez Galindo <ffernandezgalindo@triumf.ca>
2018-06-08 16:07:11 -0700

    Removed Warning category.

--- ugrstoragestats.py
+++ ugrstoragestats.py
@@ -843,7 +843,7 @@ def create_free_space_request_content():
 
 def warning_on_one_line(message, category, filename, lineno, file=None, line=None):
     #return '%s:%s: %s: %s\n' % (filename, lineno, category.__name__, message)
-    return '%s: %s\n' % (category.__name__, message)
+    return '%s\n' % (message)
 
 #############
 # Self-Test #
