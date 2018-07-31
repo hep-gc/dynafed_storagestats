a44d418948c7a666d78b9b7459b2dab632e43243
Fernando Fernandez Galindo <ffernandezgalindo@triumf.ca>
2018-07-31 16:27:55 -0700

    Added the loggers

--- doc/templates/StorageStats_subclass.py
+++ doc/templates/StorageStats_subclass.py
@@ -15,6 +15,10 @@ class NewTypeStorageStats (StorageStats):
         Extend or replace any object attributes specific to the type of
         storage endpoint. Below are the most common ones, but add as necessary.
         """
+        ############# Creating loggers ################
+        flogger = logging.getLogger(__name__)
+        mlogger = logging.getLogger('memcached_logger')
+        ###############################################
         # First we call the super function to initialize the initial atributes
         # given by the StorageStats class.
         super().__init__(*args, **kwargs)
@@ -38,9 +42,11 @@ class NewTypeStorageStats (StorageStats):
         try:
             self.validate_plugin_settings()
         except UGRConfigFileError as ERR:
+            flogger.error("[%s]%s" % (self.id, ERR.debug))
+            mlogger.error("%s" % (ERR.message))
             print(ERR.debug)
             self.debug.append(ERR.debug)
-            self.status = ERR.message
+            self.status = memcached_logline.contents()
 
         # Add any other attributes needed for this subclass.
 
@@ -51,6 +57,10 @@ class NewTypeStorageStats (StorageStats):
             Ideally we need to assing values to the following attributes, either
             obtained from the endpoint, from the endpoints.conf file or defaults
             """
+            ############# Creating loggers ################
+            flogger = logging.getLogger(__name__)
+            mlogger = logging.getLogger('memcached_logger')
+            ###############################################
             self.bytesfree = 0
             self.bytesused = 0
             self.quota = 0
@@ -63,12 +73,20 @@ class NewTypeStorageStats (StorageStats):
             protocol schema in the URN that requires some logic to figure out.
             Example below if for the DAVStorageStats.
             """
+            ############# Creating loggers ################
+            flogger = logging.getLogger(__name__)
+            mlogger = logging.getLogger('memcached_logger')
+            ###############################################
+
             schema_translator = {
                 'dav': 'http',
                 'davs': 'https',
             }
 
+            flogger.debug("[%s]Validating URN schema: %s" % (self.id, scheme))
             if scheme in schema_translator:
-                return (schema_translator[scheme])
+                flogger.debug("[%s]Using URN schema: %s" % (self.id, schema_translator[scheme]))
+                return schema_translator[scheme]
             else:
-                return (scheme)
+                flogger.debug("[%s]Using URN schema: %s" % (self.id, scheme))
+                return scheme
