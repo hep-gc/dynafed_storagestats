96a6fbba2a37a029f8cd12c3560fe06706e00ed6
Fernando Fernandez Galindo <ffernandezgalindo@triumf.ca>
2018-12-11 12:21:34 -0800

    Added While loop to allow using the marker directive to list more than 5000 blobs.

--- dynafed_storagestats/azure/helpers.py
+++ dynafed_storagestats/azure/helpers.py
@@ -32,6 +32,8 @@ def list_blobs(storage_share):
     )
 
     _container_name = storage_share.uri['container']
+    _next_marker = None
+    _timeout = int(storage_share.plugin_settings['conn_timeout'])
 
     _logger.debug(
         "[%s]Requesting storage stats with: URN: %s API Method: %s Account: %s Container: %s", \
@@ -41,45 +43,62 @@ def list_blobs(storage_share):
         storage_share.uri['container']
     )
 
-    try:
-        _blobs = _base_blob_service.list_blobs(
-            _container_name,
-            timeout=int(storage_share.plugin_settings['conn_timeout'])
-        )
+    while True:
+        try:
+            _blobs = _base_blob_service.list_blobs(
+                _container_name,
+                marker=_next_marker,
+                timeout=_timeout,
+            )
 
-    except azure.common.AzureMissingResourceHttpError as ERR:
-        raise exceptions.DSSErrorAzureContainerNotFound(
-            error='ContainerNotFound',
-            status_code="404",
-            debug=str(ERR),
-            container=_container_name,
-        )
+        except azure.common.AzureMissingResourceHttpError as ERR:
+            raise exceptions.DSSErrorAzureContainerNotFound(
+                error='ContainerNotFound',
+                status_code="404",
+                debug=str(ERR),
+                container=_container_name,
+            )
 
-    except azure.common.AzureHttpError as ERR:
-        raise exceptions.DSSConnectionErrorAzureAPI(
-            error='ConnectionError',
-            status_code="400",
-            debug=str(ERR),
-            api=storage_share.plugin_settings['storagestats.api'],
-        )
+        except azure.common.AzureHttpError as ERR:
+            raise exceptions.DSSConnectionErrorAzureAPI(
+                error='ConnectionError',
+                status_code="400",
+                debug=str(ERR),
+                api=storage_share.plugin_settings['storagestats.api'],
+            )
 
-    except azure.common.AzureException as ERR:
-        raise exceptions.DSSConnectionError(
-            error='ConnectionError',
-            status_code="400",
-            debug=str(ERR),
-        )
+        except azure.common.AzureException as ERR:
+            raise exceptions.DSSConnectionError(
+                error='ConnectionError',
+                status_code="400",
+                debug=str(ERR),
+            )
 
-    else:
-        for _blob in _blobs:
-            _total_bytes += _blob.properties.content_length
-            _total_files += 1
+        else:
+            try:
+                _blobs.items
+            except AttributeError:
+                storage_share.stats['bytesused'] = 0
+                break
+            else:
+                _next_marker = _blobs.next_marker
 
-        storage_share.stats['bytesused'] = _total_bytes
-        storage_share.stats['quota'] = storage_share.plugin_settings['storagestats.quota']
-        storage_share.stats['bytesfree'] = storage_share.stats['quota'] - _total_bytes
-        # Not required, but is useful for reporting/accounting:
-        storage_share.stats['filecount'] = _total_files
+                try:
+                    for _blob in _blobs:
+                        _total_bytes += _blob.properties.content_length
+                        _total_files += 1
+
+                except azure.common.AzureHttpError:
+                    pass
+
+                if not _next_marker:
+                    break
+
+    storage_share.stats['bytesused'] = _total_bytes
+    storage_share.stats['quota'] = storage_share.plugin_settings['storagestats.quota']
+    storage_share.stats['bytesfree'] = storage_share.stats['quota'] - _total_bytes
+    # Not required, but is useful for reporting/accounting:
+    storage_share.stats['filecount'] = _total_files
 
 
 # def ():
