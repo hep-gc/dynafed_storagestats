8d477adf1e90bbaf34ef6861b8a1797bc71209e2
Fernando Fernandez Galindo <ffernandezgalindo@triumf.ca>
2019-02-19 13:28:40 -0800

    Updated time functions.

--- dynafed_storagestats/helpers.py
+++ dynafed_storagestats/helpers.py
@@ -168,29 +168,40 @@ def get_connectionstats(storage_share_objects, memcached_ip='127.0.0.1', memcach
                 )
 
 
-def today():
-    """Returns today's aware datetime object at 00:00 UTC
+def now_in_utc():
+    """Returns aware datetime object of current time in UTC
 
 
     """
-    _today = datetime.datetime.now(dateutil.tz.tzutc())
-    _today = _today.replace(hour=0,minute=0,second=0,microsecond=0)
+    return datetime.datetime.now(dateutil.tz.tzutc())
 
-    return _today
 
-def mask_timestamp(timestamp, delta):
-    """Return true for timestamps earlier than the delta in days.
+def mask_timestamp_by_delta(timestamp, delta=0):
+    """Return false for timestamps later than the masking delta in days (UTC).
 
+    Checks if the timestamp given is later than the current time +/- the
+    delta given in days. When delta == 0, it uses the current date, but when
+    delta != 0 the current date is normalized to today at 00:00 UTC before
+    calculating the mask.
+
+    Attributes:
+    timestamp -- datetime aware time object.
+    delta -- integer.
 
     """
 
-    _today = today()
-    _mask =_today - datetime.timedelta(days=delta)
-
-    if timestamp < _mask:
-        return True
+    if delta == 0:
+        _mask = now_in_utc()
     else:
+        _mask = now_in_utc().replace(hour=0,minute=0,second=0,microsecond=0) \
+                - datetime.timedelta(days=delta)
+
+
+    if timestamp > _mask:
         return False
+    else:
+        return True
+
 
 def process_storagereports(storage_endpoint, args):
     """Run StorageShare.get_filelist() for storage shares in StorageEndpoint.
