8f6a35b7b046f13f3c01d8f845a60750e0c8e30f
Fernando Fernandez Galindo <ffernandezgalindo@triumf.ca>
2019-08-21 11:47:41 -0700

    Added ttl to memcache set function.

--- dynafed_storagestats/memcache.py
+++ dynafed_storagestats/memcache.py
@@ -31,7 +31,7 @@ def get(index, memcached_ip='127.0.0.1', memcached_port='11211'):
     else:
         return _memcached_content
 
-def set(index, data, memcached_ip='127.0.0.1', memcached_port='11211'):
+def set(index, data, memcached_ip='127.0.0.1', memcached_port='11211', ttl=3600):
     """Upload the data given to an index of a memcached instance.
 
     Arguments:
@@ -39,12 +39,13 @@ def set(index, data, memcached_ip='127.0.0.1', memcached_port='11211'):
     data  -- String to set into specified index.
     memcached_ip   -- memcached instance IP.
     memcahced_port -- memcached instance Port.
+    ttl -- Time to live expiry of indexed data. Default of 1 hour.
 
     """
     # Setup connection to a memcache instance
     _memcached_server = memcached_ip + ':' + memcached_port
     _memcached_client = memcache.Client([_memcached_server])
-    _memcached_result = _memcached_client.set(index, data)
+    _memcached_result = _memcached_client.set(index, data, time=ttl)
 
     if _memcached_result == 0:
         raise dynafed_storagestats.exceptions.MemcachedConnectionError()
