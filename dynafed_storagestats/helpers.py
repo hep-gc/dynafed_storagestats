65186ecba62f7079cf529955b88c2e1fe98a692b
Fernando Fernandez Galindo <ffernandezgalindo@triumf.ca>
2019-09-20 13:58:02 -0700

    Updated var names.

--- dynafed_storagestats/helpers.py
+++ dynafed_storagestats/helpers.py
@@ -519,15 +519,15 @@ def get_dynafed_storage_endpoints_from_schema(schema):
 
     _logger.info('Extracting dynafed storage endpoints from schema.')
 
-    for _schema_storage_service in schema['storage_service']:
-        for _schema_storage_share in _schema_storage_service['storage_shares']:
-            for _dynafed_endpoint in _schema_storage_share['dynafed_endpoints']:
+    for _schema_storage_service in schema['storageservice']:
+        for _schema_storage_share in _schema_storage_service['storageshares']:
+            for _dynafed_endpoint in _schema_storage_share['dynafedendpoints']:
 
                 _logger.info('Found dynafed storage endpoint: %s',
-                    _dynafed_endpoint['id']
+                    _dynafed_endpoint
                 )
 
-                _dynafed_endpoints.append(_dynafed_endpoint['id'])
+                _dynafed_endpoints.append(_dynafed_endpoint)
 
     _logger.debug(
         'List of dynafed endpoints to return: %s',
