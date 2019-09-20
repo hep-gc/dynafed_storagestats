9abe2bceef03e38e245d2d787f44f6e2ce6ff6cf
Fernando Fernandez Galindo <ffernandezgalindo@triumf.ca>
2019-09-20 11:23:25 -0700

    First commit. create_wlcg_storage_report()

---
+++ dynafed_storagestats/reports.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""Functions to generate storage reports."""
+
+import logging
+
+create_wlcg_storage_report(args):
+    """Creates json file according to the WLCG storage report format.
+
+    """
